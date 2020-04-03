4b66e25298fb012357873869ce9f42e5a555bdeb
itsmohitgoel <itsmohitgoel@gmail.com>
2020-04-03 16:13:13 +0530

    replace test image with user's selected image via URI

--- app/src/main/java/com/example/background/workers/BlurWorker.kt
+++ app/src/main/java/com/example/background/workers/BlurWorker.kt
@@ -2,20 +2,34 @@ package com.example.background.workers
 
 import android.content.Context
 import android.graphics.BitmapFactory
+import android.net.Uri
+import android.text.TextUtils
 import androidx.work.Data
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.example.background.KEY_IMAGE_URI
 import com.example.background.R
 import timber.log.Timber
+import java.lang.IllegalArgumentException
 
 class BlurWorker(ctx : Context, parames: WorkerParameters) : Worker(ctx, parames) {
 
     override fun doWork(): Result {
         val appContext = applicationContext
 
+        val resourceUri = inputData.getString(KEY_IMAGE_URI)
+
         makeStatusNotification("Blurring image", appContext)
-        try {
-            val picture = BitmapFactory.decodeResource(applicationContext.resources, R.drawable.test)
+        return try {
+//            val picture = BitmapFactory.decodeResource(applicationContext.resources, R.drawable.test)
+            if (TextUtils.isEmpty(resourceUri)) {
+                Timber.e("Invalid  input Uri")
+                throw IllegalArgumentException("Invalid input Uri")
+            }
+
+            val resolver = appContext.contentResolver
+            val picture = BitmapFactory.decodeStream(
+                    resolver.openInputStream(Uri.parse(resourceUri)))
 
             val output = blurBitmap(picture, appContext)
 
@@ -24,10 +38,10 @@ class BlurWorker(ctx : Context, parames: WorkerParameters) : Worker(ctx, parames
             // write bitmap to a temp file
             makeStatusNotification("Output is $outputUri", appContext)
 
-            return Result.success()
+            Result.success()
         } catch (throwable: Throwable) {
             Timber.e(throwable, "Error applying in blur")
-            return Result.failure()
+            Result.failure()
         }
     }
 
