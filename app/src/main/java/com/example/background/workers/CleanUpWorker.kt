57ce5d7b0fc666a2c4ca941e4d65064d19ab2da3
itsmohitgoel <itsmohitgoel@gmail.com>
2020-04-08 21:13:13 +0530

    Override and implement doWork() for CleanUpWorker

--- app/src/main/java/com/example/background/workers/CleanUpWorker.kt
+++ app/src/main/java/com/example/background/workers/CleanUpWorker.kt
@@ -3,9 +3,35 @@ package com.example.background.workers
 import android.content.Context
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.example.background.OUTPUT_PATH
+import timber.log.Timber
+import java.io.File
+import java.lang.Exception
 
 class CleanUpWorker(ctx : Context, params: WorkerParameters) : Worker(ctx, params) {
     override fun doWork(): Result {
-        TODO("Not yet implemented")
+        // Makes a notification when the work starts, and slows down the work so that
+        // it's easier to see each workRequest start, even on emulated devices.
+        makeStatusNotification("Cleaning up old temporary files", applicationContext)
+        sleep()
+
+        return try {
+            val outputDirectory = File(applicationContext.filesDir, OUTPUT_PATH)
+            if (outputDirectory.exists()) {
+                val entries = outputDirectory.listFiles()
+                for (entry in entries) {
+                    val name = entry.name
+                    if (name.isNotEmpty() && name.endsWith(".png")) {
+                        val deleted = entry.delete()
+                        Timber.i("Deleted $name - $deleted")
+                    }
+                }
+            }
+
+            Result.success()
+        } catch (exception : Exception) {
+            Timber.e(exception)
+            Result.failure()
+        }
     }
 }
